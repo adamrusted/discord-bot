d862850998d957b2f000c88e5ceb964bcd74e18b
Adam Rusted <hello@adamrusted.me>
2024-02-11 22:17:29 +0000

    Add translations

--- src/commands/utility/user.js
+++ src/commands/utility/user.js
@@ -3,11 +3,33 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("welcome")
+    .setNameLocalizations({
+      "es-ES": "recibir",
+      fr: "accueillir",
+      it: "Benvenuto",
+      ja: "いらっしゃいませ",
+      ko: "환영하다",
+      "pt-BR": "saudar",
+      uk: "вітання",
+      "zh-CN": "欢迎",
+      "zh-TW": "歡迎",
+    })
     .setDescription("Welcome a user to the server!")
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addUserOption((option) =>
       option
         .setName("user")
+        .setNameLocalizations({
+          "es-ES": "usuario",
+          fr: "utilisateur",
+          it: "utente",
+          ja: "ユーザー",
+          ko: "사용자",
+          "pt-BR": "usuário",
+          uk: "користувач",
+          "zh-CN": "用户",
+          "zh-TW": "使用者",
+        })
         .setRequired(true)
         .setDescription("The user you are welcoming to the server")
     ),
