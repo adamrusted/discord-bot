9511e864d8700c957d3ae0014a2fe4098a24de2e
Adam Rusted <hello@adamrusted.me>
2024-02-13 15:45:45 +0000

    Improvements to file structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { setChannelInfo, getLatestRelease } = require("./utils.js");
 const cron = require("node-cron");
-const { DateTime } = require("luxon");
-const path = require("node:path");
-const fs = require("node:fs");
+const {runCommand} = require('./commands/runCommand.js')
 
 const TIMEZONE = process.env.TZ_IDENTIFIER || "Europe/London";
 
@@ -12,66 +10,16 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-/* Client Startup */
+// Client Startup
 client.once(Events.ClientReady, (readyClient) => {
   readyClient.user.setStatus("dnd");
   console.log(`Logged in as ${readyClient.user.displayName}`);
 });
 
-/* Command Management */
-client.commands = new Collection();
+// Run Commands
+client.on(Events.InteractionCreate, async (interaction) => runCommand(interaction));
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
-
-/* Channel Title Update */
-
+// Channel Title Updates
 if (!!process.env.UPDATES_CRON) {
   cron.schedule(process.env.UPDATES_CRON, () => {
     console.log("Setting Stats Channel Names");
--- src/register-commands.js
+++ src/register-commands.js
@@ -15,21 +15,23 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
   // Grab all the command files from the commands directory you created earlier
   const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
+  if(!commandsPath.endsWith('.js')) {
+    const commandFiles = fs
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+      if ("data" in command && "execute" in command) {
+        commands.push(command.data.toJSON());
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+          );
+        }
+      }
     }
-  }
 }
 
 // Construct and prepare an instance of the REST module
