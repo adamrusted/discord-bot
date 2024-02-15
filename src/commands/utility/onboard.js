ecb5e432a9d2574a1f90191b92571484c7f3406a
Adam Rusted <hello@adamrusted.me>
2024-02-15 13:27:58 +0000

    Pull Role Info

--- src/commands/utility/onboard.js
+++ src/commands/utility/onboard.js
@@ -8,10 +8,17 @@ module.exports = {
             opt.setName('person')
             .setDescription('The user you would like to add to the maintainer team')
             .setRequired(true)
-        ),
+        )
+        .addRoleOption((opt) => {
+            opt.setName('role')
+            .setDescription('The role you are giving the user')
+            .setRequired(true)
+        }),
     async execute(interaction) {
         await interaction.reply({ content: 'This is a beta command', ephemeral: true})
-        const newMaintainer = await interaction.options.getUser('person')
-        await newMaintainer.send('Hey there! Welcome to the **Simple Icons Maintainer** Team!')
+        // const newMaintainer = await interaction.options.getUser('person')
+        const newRole = await interaction.options.getRole('person')
+        console.log(newRole)
+        // await newMaintainer.send('Hey there! Welcome to the **Simple Icons Maintainer** Team!')
     }
 }
