66e1f2b3cf7935972872a1a38cbf2dcf763c7972
Adam Rusted <hello@adamrusted.me>
2024-02-15 18:30:12 +0000

    Revert Onboarding to draft

--- src/commands/utility/poll.js
+++ src/commands/utility/poll.js
@@ -5,15 +5,18 @@ module.exports = {
     .setName("poll")
     .setDescription("Runs a poll in the channel you posted this")
     .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers) // Changed from 'Administrator' to 'BanMembers' to allow moderator roles to welcome.
-    .addStringOption((opt) => 
-        opt
-            .setRequired(true)
-            .setName('query')
-            .setDescription('Enter the query you would like people to respond to.')),
+    .addStringOption((opt) =>
+      opt
+        .setRequired(true)
+        .setName("query")
+        .setDescription("Enter the query you would like people to respond to.")
+    ),
   async execute(interaction) {
     const target = interaction.options.getString("poll");
-    const message = await interaction.reply({ content: `## New Poll from <@${interaction.user.id}>\n${target}`, fetchReply: true})
-    message.react('👍')
-        .then(() => message.react('👎'))
+    const message = await interaction.reply({
+      content: `## New Poll from <@${interaction.user.id}>\n${target}`,
+      fetchReply: true,
+    });
+    message.react("👍").then(() => message.react("👎"));
   },
 };
