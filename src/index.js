9d7e973b757ce118f8409c212e461744a5d0187d
Adam Rusted <hello@adamrusted.me>
2024-02-15 12:03:44 +0000

    Fix running of commands

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { setChannelInfo, getLatestRelease } = require("./utils.js");
 const cron = require("node-cron");
-const {runCommand} = require('./commands/runCommand.js')
-
-const TIMEZONE = process.env.TZ_IDENTIFIER || "Europe/London";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -18,7 +15,31 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Run Commands
 client.on(Events.InteractionCreate, async (interaction) => {
-  await runCommand(interaction)
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 // Channel Title Updates
