3a99a0bb0fd2d48ff30673d2c535f4e0aa7e6b1f
Adam Rusted <hello@adamrusted.me>
2024-02-16 15:37:42 +0000

    Beginnings of 'in scope' command

---
+++ src/commands/utility/inscope.js
@@ -0,0 +1,29 @@
+const { DateTime } = require("luxon");
+const { getGitHub } = require("../../utils/github");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("scope")
+    .setDescription("Checks to see if a brand is in scope of our project")
+    // .addStringOption((opt) =>
+    //   opt
+    //     .setName("url")
+    //     .setDescription("The URL of the brand / product you'd like to include")
+    // )
+    .addStringOption((opt) =>
+      opt
+        .setName("github")
+        .setDescription("The full URL of the GitHub repository")
+    ),
+  async execute(interaction) {
+    // const sourceUrl = await interaction.options.getString("url");
+    const sourceGitHub = await interaction.options.getString("github");
+    const { stars, releaseDate, recent } = await getGitHub(sourceGitHub);
+    if (stars > 5000 && recent) {
+      interaction.reply(
+        `In scope from GitHub with ${stars.toLocaleString()} stars and a release on ${DateTime.fromISO(
+          releaseDate
+        ).toLocaleString()}`
+      );
+    }
+  },
+};
---
+++ src/utils/github.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { DateTime } = require("luxon");
+
+const getGitHub = async (ghURL) => {
+  try {
+    const url = ghURL.toString().split("/");
+    const {
+      data: {
+        repository: {
+          stargazerCount: stars,
+          latestRelease: { createdAt: releaseDate },
+        },
+      },
+    } = await fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: `query ($RepositoryName: String!, $RepositoryOwner: String!) {
+                    repository(name: $RepositoryName, owner: $RepositoryOwner) {
+                        stargazerCount
+                        latestRelease {
+                            createdAt
+                        }
+                    }
+                }`,
+        variables: {
+          RepositoryOwner: url[3],
+          RepositoryName: url[4],
+        },
+      }),
+    }).then((val) => val.json());
+    console.log(releaseDate, stars);
+    return {
+      stars,
+      releaseDate,
+      recent:
+        DateTime.fromISO(releaseDate) > DateTime.now().minus({ months: 2 }),
+    };
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+getGitHub("https://github.com/simple-icons/simple-icons");
+
+module.exports = {
+  getGitHub,
+};
