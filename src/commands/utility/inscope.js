fb6dddbb278f80710ad387d254e5df23c978edd9
Adam Rusted <hello@adamrusted.me>
2024-02-16 15:53:17 +0000

    Finalize ghscope command

--- src/commands/utility/inscope.js
+++ src/commands/utility/inscope.js
@@ -4,28 +4,23 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("scope")
+    .setName("ghscope")
     .setDescription("Checks to see if a brand is in scope of our project")
-    // .addStringOption((opt) =>
-    //   opt
-    //     .setName("url")
-    //     .setDescription("The URL of the brand / product you'd like to include")
-    // )
     .addStringOption((opt) =>
       opt
-        .setName("github")
+        .setName("url")
+        .setRequired(true)
         .setDescription("The full URL of the GitHub repository")
     ),
   async execute(interaction) {
-    // const sourceUrl = await interaction.options.getString("url");
-    const sourceGitHub = await interaction.options.getString("github");
+    const sourceGitHub = await interaction.options.getString("url");
     const { stars, releaseDate, recent, error, repo } = await getGitHub(
       sourceGitHub
     );
     if (error) throw new Error(error);
     if (stars > 5000 && recent) {
       interaction.reply(
-        `**${repo.owner}/${
+        `<:tada:> **${repo.owner}/${
           repo.name
         }** is in scope from GitHub with **${stars.toLocaleString()}** stars and a release on **${DateTime.fromISO(
           releaseDate
@@ -35,7 +30,7 @@ module.exports = {
       interaction.reply(
         `**${repo.owner}/${
           repo.name
-        }** is not in scope with **${stars.toLocaleString()}** stars and **${DateTime.fromISO(
+        }<:cry:> ** is not in scope with **${stars.toLocaleString()}** stars and **${DateTime.fromISO(
           releaseDate
         ).toLocaleString()}** being their most recent release...`
       );
