c15b2e82a08aeb3de51542e8b5cf501f6d6fa17f
Adam Rusted <hello@adamrusted.me>
2024-02-18 08:41:10 +0000

    Update latest command

--- src/commands/utility/latest.js
+++ src/commands/utility/latest.js
@@ -8,11 +8,12 @@ module.exports = {
     .setDescription("Posts the latest release of the main project."),
   async execute(int) {
     const { data, error } = await getLatestStats();
+    if (error) throw new Error(error.message);
+    console.log(data);
     const postedDate = DateTime.fromISO(
       data.latestRelease.publishedAt,
     ).toFormat("dd LLL yy H:mm ZZZZ");
-    if (error) throw new Error(error.message);
-    int.reply(
+    await int.reply(
       `The latest release is [${data.latestRelease.tagName}](${data.latestRelease.url}), and released ${postedDate}.`,
     );
   },
